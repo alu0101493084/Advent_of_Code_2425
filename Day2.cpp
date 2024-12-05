e5940abd2ec99b630a07addeb9242227241d5e00
DIEGO ANTONIO <alu0101493084@cc1121.etsii.ull.es>
2024-12-05 16:54:44 +0000

    Terminado Día 2 y Día 3 comenzado

--- Day2.cpp
+++ Day2.cpp
@@ -4,7 +4,7 @@
 #include <sstream>
 
 bool IsSafe(std::vector<int>& report) {
-  int mode, difference;
+  int mode, difference = 0;
   if (report[0] > report[1]) {
     mode = -1;
   } else if (report[0] < report[1]) {
@@ -12,15 +12,15 @@ bool IsSafe(std::vector<int>& report) {
   } else {
     return false;
   }
-  for (int i = 1; i < (report.size() - 2); i++) {
-    if (mode == 1 && report[i] < report[i + 1]) {
+  for (int i = 0; (i + 1) < report.size(); i++) {
+    if ((mode == 1) && (report[i] < report[i + 1])) {
       difference = report[i + 1] - report[i];
-    } else if (mode == -1 && report[i] > report[i + 1]) {
+    } else if ((mode == -1) && (report[i] > report[i + 1])) {
       difference = report[i] - report[i + 1];
     } else {
       return false;
     }
-    if (difference < 1 || difference > 3) {
+    if (difference > 3) {
       return false;
     }
   }
@@ -29,20 +29,31 @@ bool IsSafe(std::vector<int>& report) {
 
 int main() {
   int level;
-  std::vector<int> report_vector;
+  std::vector<int> report_vector, possible_report_vector;
   std::fstream file;
   std::string report;
-  std::istringstream levels_input;
-  bool safe = false;
   int result = 0;
   file.open("input.txt", std::ios_base::in);
   while (getline(file, report)) {
-    levels_input.str(report);
+    std::istringstream levels_input(report);
     while (levels_input >> level) {
       report_vector.push_back(level);
     }
     if (IsSafe(report_vector)) {
       result++;
+    } else  {
+      for (int i = 0; i < report_vector.size(); i++) {
+        for (int j = 0; j < report_vector.size(); j++) {
+          if (j != i) {
+            possible_report_vector.push_back(report_vector[j]);
+          }
+        }
+        if (IsSafe(possible_report_vector)) {
+          result++;
+          break;
+        }
+        possible_report_vector.clear();
+      }
     }
     report_vector.clear();
     levels_input.clear();
