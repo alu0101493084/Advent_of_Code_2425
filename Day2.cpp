455eafd6a719b27d48a0bb9da7a39be8f29706a4
Diego Antonio Pi Arteaga <alu0101493084@ull.edu.es>
2024-12-05 14:16:29 +0000

    Seguir intentando el Día 2

--- Day2.cpp
+++ Day2.cpp
@@ -1,42 +1,50 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include <sstream>
 
-bool check_progression(std::istringstream& levels_input, int level, bool increasing) {
-  int next_level;
-  levels_input >> next_level;
-  int difference_increase = level - next_level;
-  int difference_decrease = next_level - level;
-  if (levels_input.rdbuf()->in_avail() == 0) {
-    return true;
-  } else if ((level > next_level) && (difference_increase <= 3) && (difference_increase >= 1)) {
-    return check_progression(levels_input, next_level, 0);
-  } else if ((level < next_level) && (difference_decrease <= 3) && (difference_decrease >= 1)) {
-    return check_progression(levels_input, next_level, 1);
+bool IsSafe(std::vector<int>& report) {
+  int mode, difference;
+  if (report[0] > report[1]) {
+    mode = -1;
+  } else if (report[0] < report[1]) {
+    mode = 1;
+  } else {
+    return false;
   }
-  return false;
+  for (int i = 1; i < (report.size() - 2); i++) {
+    if (mode == 1 && report[i] < report[i + 1]) {
+      difference = report[i + 1] - report[i];
+    } else if (mode == -1 && report[i] > report[i + 1]) {
+      difference = report[i] - report[i + 1];
+    } else {
+      return false;
+    }
+    if (difference < 1 || difference > 3) {
+      return false;
+    }
+  }
+  return true;
 }
 
 int main() {
+  int level;
+  std::vector<int> report_vector;
   std::fstream file;
   std::string report;
-  int level, initial_level, difference_increase, difference_decrease;
   std::istringstream levels_input;
-  bool safe;
-  int result;
+  bool safe = false;
+  int result = 0;
   file.open("input.txt", std::ios_base::in);
   while (getline(file, report)) {
     levels_input.str(report);
-    levels_input >> initial_level;
-    levels_input >> level;
-    difference_increase = level - initial_level;
-    difference_decrease = initial_level - level;
-    if ((level > initial_level) && (difference_increase <= 3) && (difference_increase >= 1)) {
-      safe = check_progression(levels_input, initial_level, 0);
-    } else if ((level < initial_level) && (difference_decrease <= 3) && (difference_decrease >= 1)) {
-      safe = check_progression(levels_input, initial_level, 1);
+    while (levels_input >> level) {
+      report_vector.push_back(level);
     }
-    result += safe;
+    if (IsSafe(report_vector)) {
+      result++;
+    }
+    report_vector.clear();
     levels_input.clear();
   }
   std::cout << result << std::endl;
